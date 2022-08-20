da027434dcb332cc1e9b18c92f2b69dd668fa6bb
Tadesse Adhanom <103166432+Tadhanom95@users.noreply.github.com>
2022-08-20 18:35:57 +0000

    update ejs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var data = require('./data/test.json');
 
 app.set('view engine','ejs')
 
@@ -12,7 +13,7 @@ app.use(express.static(__dirname));
 // index page
 app.get('/', (req, res) => {
   var title = 'About Me'
-  var heading = 'Tadesse Adhanom Year Up student'
+  var heading = 'Tadesse Adhanom'
   res.render('pages/index',{
     'title':title,
     'heading':heading
@@ -30,13 +31,37 @@ app.get('/practices', (req, res) => {
 
 app.get('/about', (req, res) => {
   var title = 'Skills';
-  var heading = 'JavaScript and ReactJs'
+  var heading = 'Links'
   res.render('pages/about',{
     'title':title,
-    'heading':heading
+    'heading':heading,
   })
 })
 
+app.get('/users', (req, res) => {
+  var title = 'My Users Page';
+  var heading = 'My Website'
+  res.render('users/index',{
+    'title':title,
+    'heading':heading,
+    'users':data
+  })
+})
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+  var heading ="My Website";
+ res.render('users/view', {
+     title: title,
+   'heading':heading,
+     user: data[--id]
+ })
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console. log(`Example app listening on port ${port}`)
+  console.log(data);
 })
